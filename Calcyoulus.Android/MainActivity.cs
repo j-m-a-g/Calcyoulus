3302bba5b500a3cace1483d489307c64fba42397
j-m-a-g <jmaghamez@gmail.com>
2024-06-22 15:09:34 -0400

    Finished VolumeFormulasMaterialCard in the Quick Formulas and started implementing capability to switch between in-app pages from the code-behind

--- Calcyoulus.Android/MainActivity.cs
+++ Calcyoulus.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.OS;
 namespace Calcyoulus.Droid
 {
     [Activity(Label = "Calcyoulus", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle? savedInstanceState)
         {
--- Calcyoulus/MainPage.xaml.cs
+++ Calcyoulus/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Calcyoulus
+﻿using Xamarin.Forms;
+
+namespace Calcyoulus
 {
 	public partial class MainPage
 	{
@@ -6,5 +8,53 @@
 		{
 			InitializeComponent();
 		}
-	}
+
+		// METHODS
+
+		private void SwitchScrollViewPages(ScrollView currentScrollView)
+		{
+			// Check to see if the ScrollView page is visible in the first place.
+			switch (HomePageScrollView.IsVisible)
+			{
+				case true:
+					HomePageScrollView.IsVisible = false;
+					break;
+				case false:
+					break;
+			}
+			switch (GeometryCalculatorsPageScrollView.IsVisible)
+			{
+				case true:
+					GeometryCalculatorsPageScrollView.IsVisible = false;
+					break;
+				case false:
+					break;
+			}
+			switch (LinearCalculatorsScrollView.IsVisible)
+			{
+				case true:
+					LinearCalculatorsScrollView.IsVisible = false;
+					break;
+				case false:
+					break;
+			}
+			switch (SettingsPageScrollView.IsVisible)
+			{
+				case true:
+					SettingsPageScrollView.IsVisible = false;
+					break;
+				case false:
+					break;
+			}
+
+			currentScrollView.IsVisible = true;
+		}
+
+		// EVENT HANDLERS
+
+		private void HomeNavigationPageImageButton_Clicked(object sender, System.EventArgs e)
+		{
+			SwitchScrollViewPages(HomePageScrollView);
+        }
+    }
 }
